6dcb42b1bec4d3967f59ab0a750583ceedd29f61
iqbal2906 <maniacjoe3@gmail.com>
2021-04-13 17:36:39 +0700

    Home screen

--- Airbnb/App.js
+++ Airbnb/App.js
@@ -7,7 +7,6 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,7 +25,11 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+import Entype from 'react-native-vector-icons';
+
+import HomeScreen from './src/screens/Home';
+
+const Section = ({children, title}): React$Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -52,7 +55,7 @@ const Section = ({children, title}): Node => {
   );
 };
 
-const App: () => Node = () => {
+const App: () => React$Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -60,53 +63,13 @@ const App: () => Node = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <SafeAreaView style={backgroundStyle}>
+        <HomeScreen />
+      </SafeAreaView>
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ Airbnb/src/screens/Home/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, ImageBackground, Text, Pressable} from 'react-native';
+import styles from './styles';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Pressable
+        style={styles.searchButton}
+        onPress={() => console.warn('Pressed')}>
+        <Fontisto name="search" size={25} color={'#'} />
+        <Text style={styles.searchButtonText}>Where are you going?</Text>
+      </Pressable>
+      <ImageBackground
+        source={require('../../../assets/images/wallpaper.jpg')}
+        style={styles.image}>
+        <Text style={styles.title}>Go Near</Text>
+        <Pressable
+          style={styles.button}
+          onPress={() => console.warn('Pressed')}>
+          <Text style={styles.buttonText}>Explore nearby stays</Text>
+        </Pressable>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default HomeScreen;
